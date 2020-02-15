58a46d06c9f276a918a211b1437b45707cfda97c
yago-codev <yagomilano92@gmail.com>
2020-02-15 20:23:04 -0300

    Devo criar arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -5,22 +5,12 @@ const consign = require('consign');
 // portanto o cwd foi setado como sendo a pasta a partir de onde ele deverá buscar
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
-});
-
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'John Dow', email: 'john@mail.com' },
-  ];
-
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
 });
 
 module.exports = app;
