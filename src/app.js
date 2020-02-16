765ca40952b0f8ea318419a569fe6a411ad27aba
yago-codev <yagomilano92@gmail.com>
2020-02-15 21:50:58 -0300

    Devo retatorar os testes da rota /users para usar banco de dados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const app = require('express')();
 const consign = require('consign');
+const knex = require('knex');
+const knexfile = require('../knexfile');
+
+app.db = knex(knexfile.test);
 
 // por padrão o consign busca os arquivos a partir da raíz do projeto,
 // portanto o cwd foi setado como sendo a pasta a partir de onde ele deverá buscar
---
+++ test/routes/user.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+test('Deve listar todos os usuários', () => {
+  return request(app).get('/users').then(res => {
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
+
+// test.skip = vai ignorar o teste
+// test.only = vai executar somente esse teste
+test.only('Deve inserir usuário com sucesso', () => {
+  // o Date.now() servirá pra pegar o tempo atual em milisegundos,
+  // com isso iremos garantir que os emails nunca irão se repetir
+  const email = `${Date.now()}@mail.com`;
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', email, password: '123456' })
+    .then(res => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Walter Mitty');
+    });
+});
